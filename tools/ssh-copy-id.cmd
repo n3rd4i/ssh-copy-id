e37040db315ffc76dc7586f20cea82e93a5927b1
Andrei Hutanu <andrei.hutanu@evalan.com>
2020-03-01 11:47:17 +0100

    added draft version of ssh-copy-id

--- tools/ssh-copy-id.cmd
+++ tools/ssh-copy-id.cmd
@@ -1,30 +1,27 @@
-REM source: https://stackoverflow.com/a/3981086/2692448
 @ECHO OFF
-SET man1=%1
-SET man2=%2
-SHIFT & SHIFT
+IF NOT "%1"=="" (
+    SET machine=%1
+    SHIFT
+) ELSE (
+    ECHO target machine not specified, exiting
+    ECHO.
+    ECHO Command format: ssh-copy-id [user@]machine [-i [identity_file]]
+    GOTO :theend
+)
 
+SET identity_file=%userprofile%\.ssh\id_rsa.pub
 :loop
 IF NOT "%1"=="" (
-    IF "%1"=="-username" (
-        SET user=%2
-        SHIFT
-    )
-    IF "%1"=="-otheroption" (
-        SET other=%2
+    IF "%1"=="-i" (
+        SET i_file=%2
         SHIFT
     )
     SHIFT
     GOTO :loop
 )
-ECHO Man1 = %man1%
-ECHO Man2 = %man2%
-ECHO Username = %user%
-ECHO Other option = %other%
-REM ...do stuff here...
 
-REM Source: https://linux.die.net/man/1/ssh-copy-id
-REM ssh-copy-id [-i [identity_file]] [user@]machine
+set linux_cmd="umask 077; test -d .ssh || mkdir .ssh ; cat >> .ssh/authorized_keys"
+type %identity_file% | ssh %machine% %linux_cmd%
 
 REM Source: https://linux.die.net/man/1/ssh-copy-id
 REM set src_pub_key=%userprofile%\.ssh\id_rsa.pub
